297d5a993b5e5dd87f1734c47eb97a3c11c51c7c
usama-pixel <usamaali052@gmail.com>
2022-08-29 05:12:33 +0500

    Skeleton of project created

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import 'bulma/css/bulma.min.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="button is-rounded is-loading is-primary is-responsive">Primary</button>
+    </div>
+  );
+}
+
+export default App;
